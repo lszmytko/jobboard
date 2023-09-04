b8958d655416b997c2616358de7c4bfc218300ac
Łukasz Szmytko <lukasz.szmytko@grupawp.pl>
2023-09-04 19:35:31 +0200

    feat(main): changes in AdCard componentstyles

--- src/app/_components/AdCard/AdCard.tsx
+++ src/app/_components/AdCard/AdCard.tsx
@@ -3,18 +3,19 @@ import Link from "next/link";
 const AdCard = () => {
   return (
     <Link href="/">
-      <div className="max-w-sm w-1/2 lg:max-w-full border-orange-300 border-2 rounded-lg text-sky-900 bg-gray-100 shadow-xl">
+      <div className="max-w-lg w-75 border-orange-300 border-2 rounded-lg text-sky-900 bg-gray-100 shadow-xl overflow-hidden">
         <div className="p-4">
           <p className="text-xl font-bold mb-4">Technik weterynarii</p>
-          <div className="text-xs flex gap-2 mb-4">
+          <div className="text-xs flex gap-2 mb-4 flex-wrap">
             <p>VetTech sp. z. o.o.</p>
             <p className="font-semibold">Warszawa</p>
+            <p>u. Szlenkierów 6/1</p>
           </div>
-          <div className="flex text-xs gap-2">
-            <p>Kierownik</p>
-            <p>1/3 lat doświadczenia</p>
-            <p>UoP</p>
-            <p>pełny etat</p>
+          <div className="md:flex flex-wrap text-xs gap-2 md:gap-4">
+            <p className="mb-1 md:mb-0">Kierownik</p>
+            <p className="mb-1 md:mb-0 font-bold">1/3 lat doświadczenia</p>
+            <p className="mb-1 md:mb-0">UoP</p>
+            <p className="mb-1 md:mb-0">pełny etat</p>
           </div>
         </div>
         <div className="h-px bg-orange-300"></div>
