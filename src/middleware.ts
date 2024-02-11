511d32140928355abb2fcff66c0f6cbd53e9bd2b
Lukasz Szmytko <lszmytko@lszmytko-MBP16.local>
2024-02-11 11:51:40 +0100

    feat(main): changes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { verifyJwtToken } from "./app/api/utils";
 
+const adminRoutes = [
+  "/api/editoffer",
+  "/api/editworkeroffer",
+  "/api/deleteoffer",
+  "/api/deleteworkeroffer",
+];
+
 export async function middleware(req: NextRequest, res: NextResponse) {
   const token = req.cookies.get("jwtToken")?.value;
 
   const isAuthenticated = await verifyJwtToken(token ?? "");
 
+  console.log("jestesmy tutaj", req.nextUrl.pathname);
   if (!isAuthenticated) {
+    if (adminRoutes.includes(req.nextUrl.pathname)) {
+      return Response.json(
+        { success: false, message: "authentication failed" },
+        { status: 401 }
+      );
+    }
+
     const url = req.nextUrl.clone();
     url.pathname = "/adminlogin";
     url.searchParams.delete("page");
     return NextResponse.redirect(url);
   }
 
-  NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/adminpanel/:path*",
+  matcher: ["/adminpanel/:path*", ...adminRoutes],
 };
