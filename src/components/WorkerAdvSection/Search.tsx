5cd5ccc8190183f87c4ba4416ce0ffbc2632c629
Lukasz Szmytko <lszmytko@lszmytko-MBP16.local>
2024-02-25 23:47:57 +0100

    feat(main): change width of WorkerAdvSection

--- src/components/WorkerAdvSection/Search.tsx
+++ src/components/WorkerAdvSection/Search.tsx
@@ -19,30 +19,32 @@ const Search = ({
     setFilterCriteria(data.criterium);
   };
   return (
-    <div className="mb-4">
-      <form
-        className="mb-6 mt-4 md:flex justify-center "
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <input
-          type="text"
-          placeholder="Wpisz miasto"
-          className="w-full text-center max-sm:border-b-2 p-2 border-primary-extra-light focus:outline-none md:border-r-2"
-          {...register("criterium")}
-        />
-        <input
-          type="submit"
-          value="Wyszukaj"
-          className="w-full text-center rounded py-2 px-4 bg-primary-light cursor-pointer"
-        />
-      </form>
-      <div className="flex justify-center">
-        <button
-          onClick={resetCriteria}
-          className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+    <div className="flex justify-center">
+      <div className="mb-4 w-4/5 sm:w-full">
+        <form
+          className="mb-6 mt-4 md:flex justify-center "
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Resetuj kryteria
-        </button>
+          <input
+            type="text"
+            placeholder="Wpisz miasto"
+            className="w-full text-center max-sm:border-b-2 p-2 border-primary-extra-light focus:outline-none md:border-r-2"
+            {...register("criterium")}
+          />
+          <input
+            type="submit"
+            value="Wyszukaj"
+            className="w-full text-center rounded py-2 px-4 bg-primary-light cursor-pointer"
+          />
+        </form>
+        <div className="flex justify-center">
+          <button
+            onClick={resetCriteria}
+            className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            Resetuj kryteria
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/WorkerAdvSection/WorkerAdvSection.tsx
+++ src/components/WorkerAdvSection/WorkerAdvSection.tsx
@@ -46,7 +46,7 @@ const WorkerAdvSection = () => {
   return (
     <>
       <div className="flex justify-center mt-4">
-        <section className="max-w-2xl w-4/5">
+        <section className="max-w-2xl w-full sm:w-4/5">
           <Search setFilterCriteria={setFilterCriteria} />
           <div className="flex justify-center p-2">
             <div className="w-full">
