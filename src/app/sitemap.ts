184fa8464289f463c1c0bc5b45a9e2fc25819dad
Lukasz Szmytko <lszmytko@lszmytko-MBP16.local>
2024-03-07 22:33:49 +0100

    feat(main): create sitemap and robots files

---
+++ src/app/sitemap.ts
@@ -0,0 +1,57 @@
+import { apiRoutes, paths } from "@/common/paths";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}${paths.getAllOffers}`
+  );
+
+  const { offers } = await response.json();
+
+  const offersEntries = offers.map(({ _id }: { _id: string }) => {
+    return {
+      url: `https://vetpraca.vettech.pl/advdetails/${_id}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    };
+  });
+
+  const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}${apiRoutes.getAllWorkerOffers}?isActive=true&page=1`;
+
+  const workerResponse = await fetch(url);
+
+  const { offers: workerOffers } = await workerResponse.json();
+
+  const workerOffersEntries = workerOffers.map(({ id }: { id: string }) => {
+    return {
+      url: `https://vetpraca.vettech.pl/pracownik/${id}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    };
+  });
+
+  return [
+    {
+      url: "https://vetpraca.vettech.pl",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://vetpraca.vettech.pl/pracodawca",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vetpraca.vettech.pl/pracownik",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...offersEntries,
+    ...workerOffersEntries,
+  ];
+}
