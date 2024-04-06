3cdb960e3828d48148f72f8afa2c82d973e0dedf
Lukasz Szmytko <lszmytko@lszmytko-mbp16.home>
2024-04-06 11:22:34 +0200

    fix(main): fix phone number functionality

--- src/app/(user)/advdetails/[id]/AdvCardDetails/PhoneButton/PhoneButton.tsx
+++ src/app/(user)/advdetails/[id]/AdvCardDetails/PhoneButton/PhoneButton.tsx
@@ -8,6 +8,7 @@ export default function PhoneButton({ phoneNumber }: { phoneNumber: string }) {
   const [isNumberRevealed, setIsNumberRevealed] = useState(false);
 
   const info = !isNumberRevealed ? "Odkryj numer telefonu" : phoneNumber;
+  const isAnchorClickable = isNumberRevealed ? "" : "pointer-events-none";
 
   const handleClick = () => {
     track("phone_button_click");
@@ -21,7 +22,9 @@ export default function PhoneButton({ phoneNumber }: { phoneNumber: string }) {
       id="gtm-phone-button"
       disabled={isNumberRevealed}
     >
-      {info}
+      <a href={`tel:${phoneNumber}`} className={`block ${isAnchorClickable}`}>
+        {info}
+      </a>
     </button>
   );
 }
