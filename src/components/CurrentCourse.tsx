9507a26ebacc4740f738992b8caefa14431e257c
Lukasz Szmytko <lszmytko@lszmytko-MBP16.local>
2024-04-15 23:20:41 +0200

    feat(main): change image

--- src/components/CurrentCourse.tsx
+++ src/components/CurrentCourse.tsx
@@ -1,22 +1,13 @@
 import Image from "next/image";
 import React from "react";
 
-const START_COURSE_DATE = new Date("April 15, 2024 10:00:00");
-
 const CurrentCourse = () => {
-  const currentDate = new Date();
-
-  if (currentDate > START_COURSE_DATE) return null;
-
   return (
     <div className="mb-4 w-full cursor-pointer relative flex justify-center">
-      <a
-        href="https://www.vettech.pl/usg-krok-po-kroku-modul-1"
-        className="relative"
-      >
+      <a href="https://www.vettech.pl" className="relative">
         <Image
           src="/currentCourse.png"
-          alt="Kurs anastezjologia"
+          alt="Kursy weterynaria"
           width={672}
           height={300}
         />
