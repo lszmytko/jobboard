9964e24f292296799b9b4b32eabe1398cfaf71bf
Lukasz Szmytko <lszmytko@lszmytko-MBP16.local>
2024-04-26 23:21:04 +0200

    feat(main): changes in loading

--- src/components/loaders/FullPageLoader.tsx
+++ src/components/loaders/FullPageLoader.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-import { ThreeDots } from "react-loader-spinner";
-
 const FullPageLoader = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <ThreeDots
-        height="120"
-        width="120"
-        radius="9"
-        color={"#fdba74"}
-        ariaLabel="three-dots-loading"
-        visible={true}
-      />
+      <p className="text-center font-bold">Ładowanie ...</p>
     </div>
   );
 };
